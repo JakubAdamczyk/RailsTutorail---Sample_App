354063bd331b8488a6a64ab02f91f02b2c269e83
JakubAdamczyk <jakub.spike@gmail.com>
2014-10-07 21:33:02 +0200

    update views and preper to remove duplication, bring back application.html.erb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,13 +30,13 @@ describe "Static pages" do
 
   describe "About page" do
 
-    it "should have the content 'About Us" do
+    it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
   
     it "should have the the title 'About Us'" do
-      visit '/static_pages/home'
+      visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
