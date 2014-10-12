b32bcde2eca3b019c80b03fb8a9203d992fb517c
JakubAdamczyk <jakub.spike@gmail.com>
2014-10-12 17:20:27 +0200

    Add helper tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,1 @@
-def full_title(page_title)
-  base_titel = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_titel
-  else
-    "#{base_titel} | #{page_title}"
-  end
-end
+include ApplicationHelper
