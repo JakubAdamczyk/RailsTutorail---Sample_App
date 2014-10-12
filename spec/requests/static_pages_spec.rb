aedd18ad8c914528e54a9d809b1f6844400602d8
JakubAdamczyk <jakub.spike@gmail.com>
2014-10-12 16:32:55 +0200

    Refactor rspec test to avoide duplication

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,33 +5,46 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector("h1", text: heading)  }
+    it { should have_title(full_title(page_title))  }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title(''))     }
-    it { should_not have_title('| Home')       }
+    let(:heading)     { "Sample App" }
+    let(:page_title)  { "" }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home')   }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help')       }
-    it { should have_title(full_title('')) }
+    let(:heading)     { "Help" }
+    let(:page_title)  { "Help" }
+
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About Us')    }
-    it { should have_title(full_title(''))  }
+    let(:heading)     { "About" }
+    let(:page_title)  { "About" }
+
+    it_should_behave_like "all static pages"
   end
   
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact')     }
-    it { should have_title(full_title(''))  }
+    let(:heading)     { "Contact" }
+    let(:page_title)  { "Contact" }
+
+    it_should_behave_like "all static pages"
     end
 
 end
