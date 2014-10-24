69689537d34f1a2074dd82333ba54bda162d1416
JakubAdamczyk <jakub.spike@gmail.com>
2014-10-25 00:22:44 +0200

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
 
-  before_save { email.downcase! }
+  
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -9,4 +11,19 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+private
+
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
   it { should respond_to(:password_digest)      }
   it { should respond_to(:password)             }
   it { should respond_to(:password_confirmation)}
+  it { should respond_to(:remember_token)       }
   it { should respond_to(:authenticate)         }
 
   it { should be_valid }
@@ -112,5 +113,12 @@ end
     end
 
     it { should_not be_valid }
+  end
+
+  # TESTING SESSION TOKEN #
+
+  describe "remember token" do 
+    before { @user.save }
+    it { expect(@user.remember_token).not_to be_blank }
   end
 end
